08053a0769f02fe4734bf766f81ba2607e80cfe7
Caio Rodrigues <rfcaio90@gmail.com>
2019-06-19 13:41:57 -0300

    change project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App () {
-  return <h1>You will never win!</h1>
-}
+const App = () => <h1>You will never win!</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'))
