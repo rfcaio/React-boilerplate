68208208ad372061fda6e21ec7b1ee8a5b973b6a
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-13 23:31:58 -0300

    update devDependencies and add enviroment configuration file

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+require('dotenv/config')
+
 module.exports = {
   devServer: {
     contentBase: './dist',
     historyApiFallback: true,
     inline: true,
-    port: 8001
+    port: process.env.PORT || 8001
   },
   devtool: 'inline-source-map',
   entry: './src/index.js',
